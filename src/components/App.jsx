025a79d2aab6b8406fee8c54a65cf81a6e19b042
KyryloPopov <deecy.popov@gmail.com>
2024-02-15 01:12:11 +0200

    added localStorage support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,22 @@ import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const contacts = JSON.parse(stringifiedContacts) ?? [];
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      const stringifiendContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', stringifiendContacts);
+    }
+  }
 
   handleAddContact = formData => {
     const hasDuplicates = this.state.contacts.some(
